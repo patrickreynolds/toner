152ff2e751ec62740cf34a29c0ed02b8e31387fd
Patrick Reynolds <reynolds@adobe.com>
2015-03-18 17:25:00 -0700

    Refactor out helper methods to follow a more modelistic approach to filter parsing

--- toner.js
+++ toner.js
@@ -17,12 +17,10 @@
         return this._filter(tones);
     };
 
-    Toner.prototype.addFilters = function(selection, filter) {
-        var current = selection.style[this._browser(selection)];
-        var updated = filter.constructor === Array ? 
-                        this._insertFilters(current, filter) : 
-                        this._insertFilter(current, filter);
-        this._applyTone(selection, updated);
+    Toner.prototype.addFilters = function(selection, filters) {
+        var currentFilters = selection.style[this._browser(selection)];
+        var updatedFilters = this._zipFilters(currentFilters, filters);
+        this._applyTone(selection, updatedFilters);
     };
 
     Toner.prototype.get = function(selection, filterName) {
@@ -30,12 +28,18 @@
     };
 
     Toner.prototype.removeFilters = function(selection, filters) {
-        if (filters.constructor === Array) {
-            var currentFilters = selection.style[this._browser(selection)];
-            var updatedFilters = this._spliceFilters(currentFilters, filters);
-            this._applyTone(selection, updatedFilters);
+        var currentFilters, updatedFilters;
+        if (selection.toString() === '[object HTMLCollection]') {
+            var selectionLength = selection.length;
+            while(selectionLength--) {
+                currentFilters = selection.style[this._browser(selection)];
+                updatedFilters = this._cleanFilters(selection.style[this._browser(selection)], filters);
+                this._applyTone(selection[selectionLength], this._filter(updatedFilters));
+            }
         } else {
-            this._applyTone(selection, this._removeFilter(selection, filters));
+            currentFilters = selection.style[this._browser(selection)];
+            updatedFilters = this._cleanFilters(currentFilters, filters);
+            this._applyTone(selection, updatedFilters);
         }
     };
 
@@ -53,7 +57,6 @@
         selection.style[this._browser(selection)] = tone;
     };
 
-
     Toner.prototype._filter = function(tones) {
         var self = this;
         var filters = "";
@@ -62,74 +65,53 @@
             if (tones.hasOwnProperty(tone))
                 filters += self[tone](tones[tone]) + " ";
         }
-        return filters;
+        return filters.trim();
     };
 
     Toner.prototype._valueForFilter = function(selection, name) {
         var filters = selection.style[this._browser(selection)];
-        var value = "";
-        filters.split(" ").forEach(function(filter) {
-            if (filter.split('(')[0] === name) {
-                value = this._trimSuffix(filter.split('(')[1].slice(0, -1));
-            }
-        }, this);
+        var value = this._toneFromFilters(filters)[name];
         return (value || value === 0) ? value : 'undefined';
     };
 
-    Toner.prototype._insertFilter = function(currentFilters, filterToAdd) {
-        var found = false;
-        var updatedFilters = currentFilters.split(' ').map(function(filter) {
-            if (filter.indexOf(this._filter(filterToAdd).split('(')[0]) !== -1) {
-                found = true;
-                return this._filter(filterToAdd);
-            } else {
-                return filter;
+    Toner.prototype._zipFilters = function(currentFilters, filters) {
+        var currentTone = this._toneFromFilters(currentFilters);
+        for (var tone in currentTone) {
+            if (!(filters[tone] || filters[tone] === 0)) {
+                filters[tone] = currentTone[tone];
             }
-        }, this);
-        
-        if (!found)
-            updatedFilters.push(this._filter(filterToAdd));
-
-        return updatedFilters.join(" ");
+        }
+        return this._filter(filters);
     };
 
-    Toner.prototype._insertFilters = function(currentFilters, filtersToAdd) {
-        var updatedFilters = currentFilters.split(' ').map(function(filter) {
-            var filterName = this._filter(filtersToAdd).split('(')[0];
-            if (filter.indexOf(filterName) !== -1) {
-                var newFilterObject = {};
-                newFilterObject[filterName] = filtersToAdd[filterName];
-                delete filtersToAdd[filterName];
-                return this._filter(newFilterObject);
-            } else {
-                return filter;
-            }
-        }, this);
-
-        updatedFilters.push(this._filter(filtersToAdd));
-        return updatedFilters.join(" ");
-    };
-
-    Toner.prototype._spliceFilters = function(currentFilters, filtersToSplice) {
-        filtersToSplice.forEach(function(filter, index) {
-            currentFilters = this._spliceFilter(currentFilters, filter);
-        }, this);
-        return currentFilters;
-    };
-
-    Toner.prototype._removeFilter = function(selection, filter) {
-        return this._spliceFilter(selection.style[this._browser(selection)], filter);
-    };
-
-    Toner.prototype._spliceFilter = function(current, filter) {
-        return current.split(' ').filter(function(selection) {
-            if (selection.indexOf(filter) === -1)
-                return selection;
-        }).join(" ");
+    Toner.prototype._cleanFilters = function(currentFilters, filtersToRemove) {
+        var currentTone = this._toneFromFilters(currentFilters);
+        if (filtersToRemove.constructor === Array) {
+            filtersToRemove.forEach(function(filter) {
+                if (currentTone[filter] || currentTone[filter] === 0) {
+                    delete currentTone[filter];
+                }
+            });
+        } else {
+            delete currentTone[filtersToRemove];
+        }
+        return this._filter(currentTone);
     };
 
     Toner.prototype._toneDetails = function(tone) {
         return tone.constructor === Object ? this.createTone(tone) : tone;
+    };
+
+    Toner.prototype._toneFromFilters = function(filters) {
+        var tone = {};
+        filters.split(' ').forEach(function(filter) {
+            if (filter) {
+                var filterName = filter.split('(')[0];
+                var value = parseFloat(this._trimSuffix(filter.split('(')[1].slice(0, -1)));
+                tone[filterName] = value;
+            }
+        }, this);
+        return tone;
     };
 
     Toner.prototype._trimSuffix = function(value) {
@@ -163,21 +145,21 @@
         return (parseFloat(value) > upperBound) ? upperBound : value;
     };
 
-    // Value between 1 and 10
+    // Value between 0
     Toner.prototype.blur = function(value) {
-        value = this._checkLowerBounds(value);
+        value = this._checkLowerBounds(value, 0.0);
         return 'blur('+ value +'px)';
     };
 
-    // Value between 0 and 1
+    // Value greater than 0
     Toner.prototype.brightness = function(value) {
-        value = this._checkLowerBounds(value);
+        value = this._checkLowerBounds(value, 0.0);
         return 'brightness('+ value +'%)';
     };
 
     // Value between 0 and 200
     Toner.prototype.contrast = function(value) {
-        value = this._checkLowerBounds(value);
+        value = this._checkLowerBounds(value, 0.0);
         return 'contrast('+ value +'%)';
     };
 
@@ -202,7 +184,7 @@
         return 'opacity('+ value +'%)';
     };
 
-    // Value between 0 and 
+    // Value greater than 0
     Toner.prototype.saturate = function(value) {
         value = this._checkLowerBounds(value, 0.0);
         return 'saturate('+ value +'%)';
@@ -215,5 +197,10 @@
         return 'sepia('+ value +'%)';
     };
 
-    window.toner = new Toner();
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = new Toner();
+    }
+    else {
+        window.toner = new Toner();
+    }
 }());
